3536dfe2fc9e7eb8a46e78c610078c778c63eecd
genciiv <vaqogenci@gmail.com>
2024-12-12 20:34:41 +0100

    Installing shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default App;
